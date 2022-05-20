cc455613e90168d1514feca498bcf9ac9a1349ce
Cardo Eggert <eggert@rushstreetinteractive.com>
2022-05-20 10:04:16 +0300

    Refactor checkoutservice

--- src/main/java/kata/CheckoutService.java
+++ src/main/java/kata/CheckoutService.java
@@ -12,17 +12,14 @@ public class CheckoutService {
     }
 
     public long checkout() {
-        long sumOfItems = cart.stream().map(Item::unitPrice).reduce(0L, (subPrice, price) -> subPrice + price);
-        final long discount = getDiscount("A", 3, 20);
-        sumOfItems -= discount;
-        long countOfB = cart.stream().filter(item -> item.item().equals("B")).count();
-        sumOfItems -= ((countOfB / 2) * 15);
+        long sumOfItems = cart.stream().map(Item::unitPrice).reduce(0L, Long::sum);
+        sumOfItems -= getDiscount("A", 3, 20);
+        sumOfItems -= getDiscount("B", 2, 15);
         return sumOfItems;
     }
 
     private long getDiscount(String discountItem, int countOfItemsForDiscount, int discountPrice) {
-        long countOfA = cart.stream().filter(item -> item.item().equals(discountItem)).count();
-        final long discount = (countOfA / countOfItemsForDiscount) * discountPrice;
-        return discount;
+        long countOfDiscountItems = cart.stream().filter(item -> item.item().equals(discountItem)).count();
+        return (countOfDiscountItems / countOfItemsForDiscount) * discountPrice;
     }
 }
